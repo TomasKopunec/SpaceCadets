32a42d0c6c4f2b00a2ce90297d889e3985e480b7
TomasKopunec <32284188+TomasKopunec@users.noreply.github.com>
2020-10-09 17:23:17 +0100

    Dir creation
    Created directories for each challenge

--- test.java
+++
@@ -1,8 +0,0 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
-public class test {
-    public static void main(String[] args){
-        //Something
-    }
-}
